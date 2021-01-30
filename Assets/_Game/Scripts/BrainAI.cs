ea74cf95b60ff70fc28c89a395991c468a73e4ab
Nicholas Robson <nicholas.robson@softgames.de>
2021-01-30 13:10:32 +0100

    Add AI scripts.

---
+++ Assets/_Game/Scripts/BrainAI.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainAI : MonoBehaviour
+{
+    public LayerMask bodyLayerMask;
+    
+    public string bodyTag;
+    
+    private Transform _transform;
+    
+    private List<Body> _targetBodies;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    void OnEject()
+    {
+        var bodyGameObjects = GameObject.FindGameObjectsWithTag("Body");
+        
+        _targetBodies = new List<Body>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(_transform.position, GameManager.player);
+    }
+}
